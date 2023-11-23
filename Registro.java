fbcab26607db8fd2d8669a6bb767a04c6e0926a8
GustRegnel <128606577+GustRegnel@users.noreply.github.com>
2023-11-23 18:06:36 -0300

    Update Registro.java
    Fix pra obter o próximo elemento da tabela hash

--- Registro.java
+++ Registro.java
@@ -1,11 +1,21 @@
 public class Registro {
     private int codigo;
 
+    private Registro proximo;
+
     public Registro(int codigo) {
         this.codigo = codigo;
     }
 
     public int getCodigo() {
         return codigo;
+    }
+
+    public void setProximo(Registro proximo) {
+        this.proximo = proximo;
+    }
+
+    public Registro getProximo(){
+        return proximo;
     }
 }
